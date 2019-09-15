882c469aecc8a2f07ac077136c8e80f4e579aee0
Yuri Rodrigues Santos Silva <yurirssilva@gmail.com>
2019-09-15 18:21:59 -0300

    [Type] User e Imagem

---
+++ app/GraphQL/Type/ImagemType.php
@@ -0,0 +1,57 @@
+<?php
+// app/GraphQL/Type/ImagemType.php
+namespace App\GraphQL\Type;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+use App\Models\Imagem;
+
+class ImagemType extends GraphQLType
+{
+    protected $attributes = [
+        'name'        => 'Imagem',
+        'description' => 'Uma imagem',
+        'model'       => Imagem::class
+    ];
+
+    public function fields()
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'O id da imagem'
+            ],
+            'is_principal' => [
+                'type' => Type::boolean(),
+                'description' => 'Boolean para verificar se a imagem é principal'
+            ],
+            'baixa_resolucao' => [
+                'type' => Type::string(),
+                'description' => 'A imagem em baixa resolução'
+            ],
+            'alta_resolucao' => [
+                'type' => Type::string(),
+                'description' => 'A imagem em alta resolução'
+            ],
+            'link' => [
+                'type' => Type::string(),
+                'description' => 'O link da imagem'
+            ],
+            'descricao_imagem' => [
+                'type' => Type::string(),
+                'description' => 'Uma descrição da imagem'
+            ],
+        ];
+    }
+    
+    protected function resolveCreatedAtField($root, $args)
+    {
+      return (string) $root->created_at;
+    }
+  
+    protected function resolveUpdatedAtField($root, $args)
+    {
+      return (string) $root->updated_at;
+    }
+}
---
+++ app/GraphQL/Type/UserType.php
@@ -0,0 +1,69 @@
+<?php
+// app/GraphQL/Type/UserType.php
+namespace App\GraphQL\Type;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+use App\Models\User;
+
+class UserType extends GraphQLType
+{
+    protected $attributes = [
+        'name'        => 'User',
+        'description' => 'A user',
+        'model'       => User::class
+    ];
+
+    public function fields()
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'The id of a user'
+            ],
+            'name' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The name of a user'
+            ],
+            'email' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The email address of a user'
+            ],
+            'password' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The password of a user'
+            ],
+            'status' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'The status of a user'
+            ],
+            'nivel_acesso' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'O nível de acesso do usuário'
+            ],
+            'imagem' => [
+                'type' => GraphQL::type('Imagem'),
+                'description' => 'A imagem do lançamento'
+            ],
+            'created_at' => [
+                'type' => Type::string(),
+                'description' => 'Date a was created'
+            ],
+            'updated_at' => [
+                'type' => Type::string(),
+                'description' => 'Date a was updated'
+            ],
+        ];
+    }
+
+    protected function resolveCreatedAtField($root, $args)
+    {
+        return (string) $root->created_at;
+    }
+
+    protected function resolveUpdatedAtField($root, $args)
+    {
+        return (string) $root->updated_at;
+    }
+}
