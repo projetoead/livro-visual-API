40a21fe13546bf099335624fe9e2d0e045c7e613
Yuri Rodrigues Santos Silva <yurirssilva@gmail.com>
2019-09-16 04:02:29 -0300

    [Query] Atividade - Buscar atividades

---
+++ app/GraphQL/Query/AtividadeQueries.php
@@ -0,0 +1,29 @@
+<?php
+    // app/GraphQL/Type/AtividadesQueries.php
+    namespace App\GraphQL\Query;
+
+    use GraphQL;
+    use App\Models\Atividade;
+    use GraphQL\Type\Definition\Type;
+    use Rebing\GraphQL\Support\Query;
+    use GraphQL\Type\Definition\ResolveInfo;
+    use Rebing\GraphQL\Support\SelectFields;
+
+    class BuscarAtividadesQuery extends Query {
+        protected $atributes = [
+            'name' => 'buscarAtividades'
+        ];
+
+        public function type(): Type{
+            return Type::listOf(GraphQL::type('Atividade'));
+        }
+
+        public function resolve($root, $args, SelectFields $fields, ResolveInfo $info){
+            $select = $fields->getSelect();
+            $with = $fields->getRelations();
+
+            $atividades = Atividade::query();
+
+            return $atividades->get();
+        }
+    }
