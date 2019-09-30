6da418176766f7623515a87af0fd96f383501cd0
Yuri Rodrigues Santos Silva <yurirssilva@gmail.com>
2019-09-30 15:58:21 -0300

    [Mail] params

--- app/Mail/SendMailUser.php
+++ app/Mail/SendMailUser.php
@@ -6,19 +6,22 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\User;
 
 class SendMailUser extends Mailable
 {
     use Queueable, SerializesModels;
-
+    public $user;
+    public $randomid;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user, $randomid)
     {
-        //
+        $this->user = $user;
+        $this->randomid = $randomid;
     }
 
     /**
