995f2219e26020476b5c030efdbd1f6dc64155dc
Yuri Rodrigues Santos Silva <yurirssilva@gmail.com>
2019-09-30 15:16:37 -0300

    Fix

--- config/mail.php
+++ config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => ('MAIL_DRIVER', 'smtp'),
+    'driver' => 'smtp',
 
     /*
     |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => ('MAIL_HOST', 'smtp.gmail.com'),
+    'host' => 'smtp.gmail.com',
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => ('MAIL_PORT', 587),
+    'port' => 587,
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => ('MAIL_FROM_ADDRESS', 'projetoeadatividades@gmail.com'),
-        'name' => ('MAIL_FROM_NAME', 'Livro Visual'),
+        'address' => 'projetoeadatividades@gmail.com',
+        'name' => 'Livro Visual',
     ],
 
     /*
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => ('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => 'tls',
 
     /*
     |--------------------------------------------------------------------------
