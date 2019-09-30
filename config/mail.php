06f8d923750087408220075822f50f8a83f2d7d9
Yuri Rodrigues Santos Silva <yurirssilva@gmail.com>
2019-09-30 14:38:33 -0300

    [Mail] Config

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'projetoeadatividades@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Livro Visual'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('projetoeadatividades@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('cngftoyblutfphex'),
 
     /*
     |--------------------------------------------------------------------------
